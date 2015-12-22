0eb30584d4cffb48afeafb9895f34fc6043e890f
Paul Cunningham <paul@locklan.com.au>
2015-12-22 17:47:29 +1000

    Update New-LabUsers.ps1

--- New-LabUsers.ps1
+++ New-LabUsers.ps1
@@ -55,6 +55,30 @@ check out Exchange Server Pro.
 * Website:	http://exchangeserverpro.com
 * Twitter:	http://twitter.com/exchservpro
 
+License:
+
+The MIT License (MIT)
+
+Copyright (c) 2015 Paul Cunningham
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
 Change Log
 V1.00, 22/11/2015 - Initial version
 #>
@@ -109,7 +133,7 @@ $Departments = @(
 
 # Password length and character set to use for random password generation
 $PasswordLength = 16
-$ascii=$NULL;For ($a=33;$a –le 126;$a++) {$ascii+=,[char][byte]$a }
+$ascii=$NULL;For ($a=33;$a â€“le 126;$a++) {$ascii+=,[char][byte]$a }
 
 
 #...................................
@@ -134,7 +158,7 @@ Function Get-TempPassword() {
         [string[]]$sourcedata
     )
 
-    For ($loop=1; $loop –le $length; $loop++)
+    For ($loop=1; $loop â€“le $length; $loop++)
     {
         $TempPassword+=($sourcedata | Get-Random)
     }
